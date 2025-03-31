1b9c2a67616044a774537a105c897ea943c0ab72
Hsuan-Kai Liao <hl475@duke.edu>
2025-03-31 17:12:51 -0400

    SALAD-02: combine playerController to input handler

--- src/main/java/oogasalad/engine/gameComponent/InputHandler.java
+++ src/main/java/oogasalad/engine/gameComponent/InputHandler.java
@@ -1,19 +1,68 @@
 package oogasalad.engine.gameComponent;
 
-import oogasalad.engine.base.ComponentTag;
-import oogasalad.engine.base.GameComponent;
-import oogasalad.engine.base.GameAction;
-import oogasalad.engine.base.KeyCode;
+import oogasalad.engine.base.*;
 
+import java.util.List;
+
+/**
+ * This is the class that handle all the event and binds them to the specified keybindings
+ */
 public class InputHandler extends GameComponent {
     public static final ComponentTag TAG = ComponentTag.NONE;
+
+    private List<GameAction> actions;
 
     public InputHandler() {
         super(TAG);
     }
 
+    /**
+     * Register the game action to the specific keyCode
+     * @param input the input keycode
+     * @param action the action that binds to that key
+     */
     public void registerAction(KeyCode input, GameAction action) {
         getParent().getScene().getInputMapping().addMapping(input, action);
+    }
+
+    /**
+     * Add the action to this game object
+     * @param actionClass the specified game action class
+     */
+    public <T extends GameAction> void addAction(Class<T> actionClass) {
+        try {
+            actions.add(actionClass.getDeclaredConstructor().newInstance());
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to add action", e);
+        }
+    }
+
+    /**
+     * Get the game action based on the input class
+     * @param actionClass the specified game action class
+     * @return the subscribed action
+     */
+    public <T extends GameAction> T getAction(Class<T> actionClass) {
+        for (GameAction action : actions) {
+            if (action.getClass().equals(actionClass)) {
+                return (T) action;
+            }
+        }
+        throw new IllegalArgumentException("Action does not exist");
+    }
+
+    /**
+     * Remove the game action based on the specified input class
+     * @param actionClass the specified game action class
+     */
+    public <T extends GameAction> void removeAction(Class<T> actionClass) {
+        for (GameAction action : actions) {
+            if (action.getClass().equals(actionClass)) {
+                actions.remove(action);
+                return;
+            }
+        }
+        throw new IllegalArgumentException("Action does not exist");
     }
 
 }
--- src/main/java/oogasalad/engine/gameObject/Player.java
+++ src/main/java/oogasalad/engine/gameObject/Player.java
@@ -3,13 +3,11 @@ import java.util.UUID;
 import oogasalad.engine.base.GameObject;
 import oogasalad.engine.base.GameScene;
 import oogasalad.engine.gameComponent.InputHandler;
-import oogasalad.engine.gameComponent.PlayerController;
 
 public class Player extends GameObject {
 
   public Player(String name, UUID id, GameScene parentScene) {
     super(name, id, parentScene);
     addComponent(InputHandler.class);
-    addComponent(PlayerController.class);
   }
 }
