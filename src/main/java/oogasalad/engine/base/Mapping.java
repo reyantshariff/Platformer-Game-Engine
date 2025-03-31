8702646400515000f556acdaca9aa8b6d1fa3ef1
Christian Bepler <cgb45@duke.edu>
2025-03-30 21:06:14 -0400

    ENGINE-03: Create Mapping Class - closes #59

---
+++ src/main/java/oogasalad/engine/base/Mapping.java
@@ -0,0 +1,37 @@
+package oogasalad.engine.base;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Mapping {
+    private Map<String, List<GameAction>> inputActionMap;
+
+    public Mapping() {
+        inputActionMap = new HashMap<>();
+    }
+
+    /**
+     * Associates an action to an input.
+     *
+     * @param input the input to map
+     * @param actions the list of actions to associate with the input
+     */
+    public void addMapping(String input, GameAction action) {
+        inputActionMap.computeIfAbsent(input, k -> new java.util.ArrayList<>()).add(action);
+    }
+
+    /**
+     * Triggers the dispatch method on all actions associated with the input.
+     *
+     * @param input the input to trigger
+     */
+    public void trigger(String input) {
+        List<GameAction> actions = inputActionMap.get(input);
+        if (actions != null) {
+            for (GameAction action : actions) {
+                action.dispatch();
+            }
+        }
+    }
+}
