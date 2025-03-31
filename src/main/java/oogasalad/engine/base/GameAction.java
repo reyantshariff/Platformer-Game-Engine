3aa865c61a914df80d305b28614313df4fc7d8c6
Christian Bepler <cgb45@duke.edu>
2025-03-31 13:46:13 -0400

    SALAD-02: add GameActionConstraints

--- src/main/java/oogasalad/engine/base/GameAction.java
+++ src/main/java/oogasalad/engine/base/GameAction.java
@@ -1,13 +1,17 @@
 package oogasalad.engine.base;
 
+import java.util.List;
+
 /**
- * The GameAction class is the base class for all game actions. Actions are event that are called only under
- * certain conditions. For example, a player will only jump when the user presses the jump button.
+ * The GameAction class is the base class for all game actions. Actions are event that are called
+ * only under certain conditions. For example, a player will only jump when the user presses the
+ * jump button.
  */
 
 public abstract class GameAction {
 
     private GameObject parent;
+    private List<GameActionConstraint> constraints;
 
     public GameAction(GameObject parent) {
         this.parent = parent;
@@ -15,6 +19,8 @@ public abstract class GameAction {
 
     /**
      * Trigger the action
+     * 
+     * Implementations of this method should call checkConstraints before executing the body
      */
     public abstract void dispatch();
 
@@ -25,5 +31,38 @@ public abstract class GameAction {
      */
     public GameObject getParent() {
         return parent;
+    }
+
+    public void registerConstraint(Class<? extends GameActionConstraint> constraintClass) {
+        try {
+            constraints
+                    .add(constraintClass.getDeclaredConstructor(this.getClass()).newInstance(this));
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to register constraint", e);
+        }
+    }
+
+    public void unregisterConstraint(Class<? extends GameActionConstraint> constraintClass) {
+        for (GameActionConstraint constraint : constraints) {
+            if (constraint.getClass().equals(constraintClass)) {
+                constraints.remove(constraint);
+                return;
+            }
+        }
+        throw new IllegalArgumentException("Constraint does not exist");
+    }
+
+    /**
+     * Check all constraints of the action
+     * 
+     * @return true if all constraints are met, false otherwise
+     */
+    protected boolean checkConstraints() {
+        for (GameActionConstraint constraint : constraints) {
+            if (!constraint.check()) {
+                return false;
+            }
+        }
+        return true;
     }
 }
