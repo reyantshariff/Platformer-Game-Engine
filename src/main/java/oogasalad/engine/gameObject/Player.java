38043a947539e0a7175cdbf4a98e4e42dc38bfdf
Christian Bepler <cgb45@duke.edu>
2025-03-30 22:10:07 -0400

    uncomment code

--- src/main/java/oogasalad/engine/gameObject/Player.java
+++ src/main/java/oogasalad/engine/gameObject/Player.java
@@ -2,12 +2,14 @@ package oogasalad.engine.gameObject;
 import java.util.UUID;
 import oogasalad.engine.base.GameObject;
 import oogasalad.engine.base.GameScene;
+import oogasalad.engine.gameComponent.InputHandler;
+import oogasalad.engine.gameComponent.PlayerController;
 
 public class Player extends GameObject {
 
   public Player(String name, UUID id, GameScene parentScene) {
     super(name, id, parentScene);
-    //addComponent(inputHandler.class);
-    //addComponent(playerController.class)
+    addComponent(InputHandler.class);
+    addComponent(PlayerController.class);
   }
 }
