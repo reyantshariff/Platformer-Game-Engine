174fd7b20ee3fd7d15d2ec662dc2112791e91485
Justin Aronwald <jga18@duke.edu>
2025-04-01 00:01:19 -0400

    Feature PLAYER-13: fixed parser writer, added resourceParser

--- src/main/java/oogasalad/parser/SceneParser.java
+++ src/main/java/oogasalad/parser/SceneParser.java
@@ -1,23 +1,57 @@
 package oogasalad.parser;
 
 import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.NullNode;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.List;
+import javafx.scene.Scene;
 
-public class SceneParser implements Parser<Void> {
+/**
+ * Parses and serializes scenes to and from JSON
+ *
+ */
+public class SceneParser implements Parser<List<Scene>> {
   private final GameObjectParser gameObjectParser = new GameObjectParser();
+
+  /**
+   * Parses a JSON array of scenes into a list of Scene objects
+   *
+   * @param node - the JSON node given to parse
+   * @return - a list of the parsed scene objects
+   * @throws ParsingException - parsing error specific thrown if issues with parsing
+   */
   @Override
-  public Void parse(JsonNode node) throws ParsingException {
-    for (JsonNode child : node) {
-      String sceneName = child.get("Name").asText();
-      JsonNode gameObjects = child.get("GameObjects");
-      gameObjectParser.parse(gameObjects);
+  public List<Scene> parse(JsonNode node) throws ParsingException {
+    if (node == null || !node.isArray()) {
+      throw new IllegalArgumentException("SceneParser requires a JSON array");
     }
-    return null;
+
+    List<Scene> scenes = new ArrayList<>();
+
+    for (JsonNode scene : node) {
+      String sceneName = scene.get("Name").asText();
+
+      JsonNode gameObjects = scene.get("GameObjects");
+      List<Entity> entities = gameObjectParser.parse(gameObjects);
+
+      Scene newScene = new Scene(sceneName, entities);
+      scenes.add(newScene);
+
+    }
+    return scenes;
   }
 
+  /**
+   * Serializes a list of scene objects into JSON array
+   *
+   * @param data - the data object to serialize
+   * @throws IOException - the error thrown when input output fails or is interrupted
+   */
   @Override
-  public void write(Void data, OutputStream output) throws IOException {
-
+  public JsonNode write(List<Scene> data) throws IOException {
+    return NullNode.getInstance();
   }
+
 }
