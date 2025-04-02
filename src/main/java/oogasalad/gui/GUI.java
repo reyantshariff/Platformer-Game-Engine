a7e4c91d2d625b421ddae92858d1aed370681aa2
Jack Regan <jfr29@duke.edu>
2025-04-01 23:43:27 -0400

    Fix: correct format

--- src/main/java/oogasalad/gui/GUI.java
+++ src/main/java/oogasalad/gui/GUI.java
@@ -18,40 +18,42 @@ import oogasalad.engine.component.Transform;
 public class GUI {
 
   public GUI(Stage stage, Game game) {
-    ResourceBundles.loadBundle("oogasalad.gui.resources.general");
+    ResourceBundles.loadBundle("oogasalad.gui.general");
     generateGUI(stage, game);
   }
 
   private void generateGUI(Stage stage, Game game) {
+    // Initialize JavaFX Scene
     Group root = new Group();
-    Scene scene = new Scene(root, ResourceBundles.getInt("oogasalad.gui.resources.general", "windowWidth"), ResourceBundles.getInt("oogasalad.gui.resources.general", "windowHeight")); // size your game window here
-    Canvas canvas = new Canvas(ResourceBundles.getInt("oogasalad.gui.resources.general", "windowWidth"), ResourceBundles.getInt("oogasalad.gui.resources.general", "windowHeight"));
+    Scene scene = new Scene(root, ResourceBundles.getInt("oogasalad.gui.general", "windowWidth"), ResourceBundles.getInt("oogasalad.gui.general", "windowHeight")); // size your game window here
+    Canvas canvas = new Canvas(ResourceBundles.getInt("oogasalad.gui.general", "windowWidth"), ResourceBundles.getInt("ooogasalad.gui.general", "windowHeight"));
     GraphicsContext gc = canvas.getGraphicsContext2D();
     root.getChildren().add(canvas);
 
     stage.setTitle("OOGASalad Platformer");
     stage.setScene(scene);
     stage.show();
-
+    // Map Key Codes
     scene.setOnKeyPressed(e -> {
       KeyCode key = mapToEngineKeyCode(e.getCode());
       if (key != null && game.getCurrentScene() != null) {
         game.getCurrentScene().subscribeInputKey(key);
       }
     });
+    // Enter Timeline loop
     Timeline gameLoop = new Timeline();
     gameLoop.setCycleCount(Timeline.INDEFINITE);
     gameLoop.getKeyFrames().add(new KeyFrame(
-        Duration.seconds((double) 1 / ResourceBundles.getDouble("oogasalad.gui.resources.general", "framesPerSecond")),
+        Duration.seconds((double) 1 / ResourceBundles.getDouble("oogasalad.gui.general", "framesPerSecond")),
         event -> {
-          game.step((double) 1 / ResourceBundles.getDouble("oogasalad.gui.resources.general", "framesPerSecond"));
+          game.step((double) 1 / ResourceBundles.getDouble("oogasalad.gui.general", "framesPerSecond"));
           render(gc, game.getCurrentScene());
         }
     ));
     gameLoop.play();
   }
   private void render(GraphicsContext gc, GameScene scene) {
-    gc.clearRect(ResourceBundles.getInt("oogasalad.gui.resources.general", "windowX"), ResourceBundles.getInt("oogasalad.gui.resources.general", "windowY"), ResourceBundles.getDouble("oogasalad.gui.resources.general", "windowWidth"), ResourceBundles.getDouble("oogasalad.gui.resources.general", "windowHeight"));
+    gc.clearRect(ResourceBundles.getInt("oogasalad.gui.general", "windowX"), ResourceBundles.getInt("oogasalad.gui.general", "windowY"), ResourceBundles.getDouble("oogasalad.gui.general", "windowWidth"), ResourceBundles.getDouble("oogasalad.gui.general", "windowHeight"));
 
     // Simple draw loop — assume all GameObjects have Transform and (eventually) Sprite
     for (GameObject obj : scene.getAllObjects()) {
