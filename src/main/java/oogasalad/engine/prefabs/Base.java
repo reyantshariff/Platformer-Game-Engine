0420f6e0155ac3194b647b2b7d4750a3ba08626c
Logan Dracos <lsd25@duke.edu>
2025-04-02 00:08:43 -0400

    Feature SALAD-02: Added infrastructure for constraining jumping to be when in contact with ground

---
+++ src/main/java/oogasalad/engine/prefabs/Base.java
@@ -0,0 +1,10 @@
+package oogasalad.engine.prefabs;
+import oogasalad.engine.base.architecture.GameObject;
+
+public class Base extends GameObject {
+
+
+  public Base(String name) {
+    super(name);
+  }
+}
