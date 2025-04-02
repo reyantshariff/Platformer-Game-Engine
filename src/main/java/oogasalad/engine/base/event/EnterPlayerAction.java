a7d7374104b9f08a3e0448651bb497cd93f00dd8
Jack Regan <jfr29@duke.edu>
2025-04-02 14:27:26 -0400

    FEATURE SALAD-1: created a game action that switches the scene into the dinosaur player

--- src/main/java/oogasalad/engine/base/event/EnterPlayerAction.java
+++ src/main/java/oogasalad/engine/base/event/EnterPlayerAction.java
@@ -1,4 +1,19 @@
 package oogasalad.engine.base.event;
 
-public class EnterPlayerAction {
+import oogasalad.engine.base.architecture.GameObject;
+import oogasalad.player.dinosaur.DinosaurGameScene;
+import oogasalad.Main;
+
+public class EnterPlayerAction extends GameAction{
+
+  public EnterPlayerAction(GameObject parent) {super(parent);}
+  /**
+   * Trigger the action Implementations of this method should call checkConstraints before executing
+   * the body
+   */
+  @Override
+  public void dispatch() {
+    Main.game.addScene(DinosaurGameScene.class, "Dinosaur");
+    Main.game.changeScene("Dinosaur");
+  }
 }
