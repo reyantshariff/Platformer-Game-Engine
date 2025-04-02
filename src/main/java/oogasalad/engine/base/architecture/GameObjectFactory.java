6a4d8d0e3489ca36b20769a6a422fc212d1a4aa0
Reyan Shariff <rts34@duke.edu>
2025-04-01 22:57:38 -0400

    Feature BUILDER-02: Add factory class to initialize game objects

---
+++ src/main/java/oogasalad/engine/base/architecture/GameObjectFactory.java
@@ -0,0 +1,28 @@
+package oogasalad.engine.base.architecture;
+
+import java.lang.reflect.Constructor;
+
+public class GameObjectFactory {
+
+  private static final String GAME_OBJECT_PACKAGE = "oogasalad.engine.gameobject";
+
+  @SuppressWarnings("unchecked")
+  public static GameObject create(String type) {
+    try {
+      String className = GAME_OBJECT_PACKAGE + "." + type;
+
+      Class<?> clazz = Class.forName(className);
+
+      if (!GameObject.class.isAssignableFrom(clazz)) {
+        throw new IllegalArgumentException(type + " is not a valid GameObject type");
+      }
+
+      Constructor<? extends GameObject> constructor =
+          (Constructor<? extends GameObject>) clazz.getConstructor(String.class);
+
+      return constructor.newInstance(type);
+    } catch (Exception e) {
+      throw new RuntimeException("Failed to create GameObject of type: " + type, e);
+    }
+  }
+}
