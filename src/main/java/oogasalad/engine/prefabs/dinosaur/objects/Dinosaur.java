caab36c0295b53832db7586ce4d86b3bfc684c4c
Reyan Shariff <rts34@duke.edu>
2025-04-01 20:27:25 -0400

    Refactor PLAYER-10: Add Components to Dinosaur Object and move to prefabs

--- src/main/java/oogasalad/engine/prefabs/dinosaur/objects/Dinosaur.java
+++ src/main/java/oogasalad/engine/prefabs/dinosaur/objects/Dinosaur.java
@@ -1,8 +1,12 @@
-package oogasalad.games.dinosaur.objects;
+package oogasalad.engine.prefabs.dinosaur.objects;
 
 import oogasalad.engine.base.architecture.GameObject;
-import oogasalad.engine.prefabs.Player;
-import oogasalad.games.dinosaur.components.VelocityComponent;
+import oogasalad.engine.component.GravityComponent;
+import oogasalad.engine.component.InputHandler;
+import oogasalad.engine.component.Transform;
+import oogasalad.engine.component.VelocityComponent;
+
+
 
 public class Dinosaur extends GameObject {
   public Dinosaur()
@@ -20,6 +24,9 @@ public class Dinosaur extends GameObject {
   }
 
   private void addDefaultComponents() {
+    addComponent(GravityComponent.class);
     addComponent(VelocityComponent.class);
+    addComponent(InputHandler.class);
+    addComponent(Transform.class);
   }
 }
