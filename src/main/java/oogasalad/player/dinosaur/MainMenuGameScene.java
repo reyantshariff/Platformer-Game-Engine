bf6bf671bd9ac36065a99817422cca73c1571579
Jack Regan <jfr29@duke.edu>
2025-04-06 19:50:01 -0400

    SALAD-01: hardcoded test for main menu

--- src/main/java/oogasalad/player/dinosaur/MainMenuGameScene.java
+++ src/main/java/oogasalad/player/dinosaur/MainMenuGameScene.java
@@ -4,6 +4,8 @@ import oogasalad.engine.base.architecture.GameScene;
 import oogasalad.engine.base.enumerate.KeyCode;
 import oogasalad.engine.base.event.EnterPlayerAction;
 import oogasalad.engine.component.InputHandler;
+import oogasalad.engine.component.TextComponent;
+import oogasalad.engine.component.Transform;
 import oogasalad.engine.prefabs.Player;
 
 public class MainMenuGameScene extends GameScene {
@@ -17,6 +19,28 @@ public class MainMenuGameScene extends GameScene {
    */
   @Override
   public void onActivated() {
+    Player player = makePlayerTest();
+  }
+
+  private Player makePlayerTest() {
+    Player player = instantiateObject(Player.class);
+
+    Transform t = player.addComponent(Transform.class);
+    t.setX(100);
+    t.setY(100);
+    t.setScaleX(10);
+    t.setScaleY(10);
+
+    TextComponent textComponent = player.addComponent(TextComponent.class);
+    textComponent.setText("Welcome to OOOOOOOGASALAD");
+    textComponent.setX(600);
+    textComponent.setY(300);
+
+    InputHandler input = player.addComponent(InputHandler.class);
+    EnterPlayerAction enterPlayer = new EnterPlayerAction();
+    input.registerAction(KeyCode.SPACE, enterPlayer.getClass());
+
+    return player;
   }
 
   /**
