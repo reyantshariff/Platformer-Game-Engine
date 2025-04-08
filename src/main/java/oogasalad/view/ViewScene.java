6812a376678e1f35e21c0bd4cfd87d1fc3793b80
Calvin Chen <clc162@duke.edu>
2025-04-08 15:50:28 -0400

    Refactor: create abstract view class (having a GameObjectRenderer) which different types of program windows will extend.

--- src/main/java/oogasalad/view/ViewScene.java
+++ src/main/java/oogasalad/view/ViewScene.java
@@ -2,12 +2,30 @@ package oogasalad.view;
 
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import oogasalad.gui.GameObjectRenderer;
 
-public class ViewScene {
-  Scene myScene;
+public abstract class ViewScene {
+  protected final Scene myScene;
+  protected final GameObjectRenderer myObjectRenderer;
 
+  /**
+   * Template for a program JavaFX window using a GameObjectRenderer
+   *
+   * @param root define type of JavaFX window, such as BorderPane
+   * @param width window width in pixels
+   * @param height window height in pixels
+   */
   public ViewScene(Parent root, double width, double height) {
     myScene = new Scene(root, width, height);
+    myObjectRenderer = new GameObjectRenderer(myScene);
+  }
+
+  /**
+   * Return this window's Scene object
+   * @return JavaFX Scene object
+   */
+  public Scene getScene() {
+    return myScene;
   }
 
 }
