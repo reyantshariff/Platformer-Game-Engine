7bb778d47a6cd67e5db96e1c68f508513d6411ed
Hsuan-Kai Liao <hl475@duke.edu>
2025-04-08 04:05:01 -0400

    Feature SALAD-06: add some example BehaviorConstraints and BehaviorActions

---
+++ src/main/java/oogasalad/engine/action/VelocityYSetAction.java
@@ -0,0 +1,30 @@
+package oogasalad.engine.action;
+
+import oogasalad.engine.base.behavior.BehaviorAction;
+import oogasalad.engine.base.enumerate.ComponentTag;
+import oogasalad.engine.component.PhysicsHandler;
+
+/**
+ * VelocityXSetAction is a class that extends BehaviorAction and is used to set the X velocity of
+ * the object.
+ *
+ * @author Hsuan-Kai Liao
+ */
+public class VelocityYSetAction extends BehaviorAction<Double> {
+  @Override
+  public ComponentTag ActionType() {
+    return ComponentTag.PHYSICS;
+  }
+
+  private PhysicsHandler physicsHandler;
+
+  @Override
+  protected void awake() {
+    physicsHandler = getComponent(PhysicsHandler.class);
+  }
+
+  @Override
+  protected void perform(Double parameter) {
+    physicsHandler.setVelocityY(parameter);
+  }
+}
---
+++ src/main/java/oogasalad/engine/constraint/KeyPressConstraint.java
@@ -0,0 +1,31 @@
+package oogasalad.engine.constraint;
+
+import oogasalad.engine.base.behavior.BehaviorConstraint;
+import oogasalad.engine.base.enumerate.ComponentTag;
+import oogasalad.engine.base.enumerate.KeyCode;
+import oogasalad.engine.component.InputHandler;
+
+/**
+ * KeyHoldConstraint is a class that extends BehaviorConstraint and is used to check if a key is
+ * being held down.
+ *
+ * @author Hsuan-Kai Liao
+ */
+public class KeyPressConstraint extends BehaviorConstraint<KeyCode> {
+  @Override
+  public ComponentTag ConstraintType() {
+    return ComponentTag.INPUT;
+  }
+
+  private InputHandler inputHandler;
+
+  @Override
+  protected void awake() {
+    inputHandler = getComponent(InputHandler.class);
+  }
+
+  @Override
+  public boolean check(KeyCode parameter) {
+    return inputHandler.isKeyPressed(parameter);
+  }
+}
