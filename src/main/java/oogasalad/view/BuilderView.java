7f58a426a108ffdb106374df81661d9152193ed9
Calvin Chen <clc162@duke.edu>
2025-04-08 15:53:08 -0400

    Feature: create BuilderView class (which displays the level builder) and display an example dummy window via Main

--- src/main/java/oogasalad/view/BuilderView.java
+++ src/main/java/oogasalad/view/BuilderView.java
@@ -1,26 +1,47 @@
 package oogasalad.view;
 
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
-import oogasalad.builder.BuilderScene;
+import oogasalad.engine.base.architecture.GameScene;
 import oogasalad.gui.button.StylizedButton;
+import oogasalad.player.dinosaur.DinosaurGameScene;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
-public class DinosaurGameBuilderView extends BorderPane {
-  private BuilderScene sceneBackend;
+public class BuilderView extends ViewScene {
+
+  private static final Logger logger = LogManager.getLogger(BuilderView.class);
+
+  private final BorderPane myWindow;
 
   private StylizedButton loadButton;
   private StylizedButton saveButton;
   private StylizedButton playtestButton;
 
+  private Canvas canvas;
+
   private Pane editorCanvas;
   private VBox toolbox;
   private VBox propertiesPanel;
 
-  public DinosaurGameBuilderView() {
+  private GameScene gameScene;
+
+  public BuilderView(double width, double height) {
+    // Create the BorderPane as the root
+    super(new BorderPane(), width, height);
+    myWindow = (BorderPane) super.myScene.getRoot();
+    createDinoGameTest();
     initializeUI();
+  }
+
+  private void createDinoGameTest() {
+    gameScene = new DinosaurGameScene("LevelEditTest");
+    gameScene.onActivated();
   }
 
   private void initializeUI() {
@@ -30,27 +51,35 @@ public class DinosaurGameBuilderView extends BorderPane {
     saveButton = new StylizedButton("Save");
     playtestButton = new StylizedButton("Playtest");
     topBar.getChildren().addAll(loadButton.getButton(), saveButton.getButton(), playtestButton.getButton());
-    setTop(topBar);
+    myWindow.setTop(topBar);
 
     // Left toolbox
     toolbox = new VBox();
     toolbox.getChildren().add(new Label("Blocks"));
     // Add more UI controls for placing blocks
-    setLeft(toolbox);
+    myWindow.setLeft(toolbox);
 
     // Center canvas
+    canvas = new Canvas(1200, 800);
+    GraphicsContext gc = canvas.getGraphicsContext2D();
+    renderInitialFrame(gc, gameScene);
     editorCanvas = new Pane();
-    setCenter(editorCanvas);
+    editorCanvas.getChildren().add(canvas);
+    myWindow.setCenter(editorCanvas);
 
     // Right properties panel
     propertiesPanel = new VBox();
     propertiesPanel.getChildren().add(new Label("Properties"));
-    setRight(propertiesPanel);
+    myWindow.setRight(propertiesPanel);
 
     // Event handlers
 //    loadButton.setOnAction(e -> handleLoad());
 //    saveButton.setOnAction(e -> handleSave());
 //    playtestButton.setOnAction(e -> handlePlaytest());
+  }
+
+  private void renderInitialFrame(GraphicsContext gc, GameScene gameScene) {
+    myObjectRenderer.render(gc, gameScene);
   }
 
   private void handleLoad() {
@@ -68,4 +97,3 @@ public class DinosaurGameBuilderView extends BorderPane {
     // ...
   }
 }
-
