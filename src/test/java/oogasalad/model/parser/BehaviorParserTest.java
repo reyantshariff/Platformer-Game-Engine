5ab4e70d2669420afdce6dd3ddd1344ec9fe6fa1
Daniel Rodriguez-Florido <dr285@duke.edu>
2025-04-09 17:27:06 -0400

    Refactor SALAD-08: Buffed up behavior testing after new fix

--- src/test/java/oogasalad/model/parser/BehaviorParserTest.java
+++ src/test/java/oogasalad/model/parser/BehaviorParserTest.java
@@ -12,52 +12,18 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.IOException;
-import oogasalad.model.engine.base.behavior.BehaviorConstraint;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.shadow.com.univocity.parsers.common.beans.BeanHelper;
 
 class BehaviorParserTest {
 
+  static final int CONSTRAINTS_INDEX = 1;
+  static final int ACTIONS_INDEX = 2;
+
   BehaviorParser myBehaviorParser;
   ObjectMapper myMapper;
-  String goodJsonString =
-      """
-          {
-            "Name": "SceneChanger",
-            "constraints": [
-              {
-                "name": "KeyPressConstraint",
-                "parameter": "SPACE"
-              }
-            ],
-            "actions": [
-              {
-                "name": "ChangeSceneAction",
-                "parameter": "Example Main Scene"
-              }
-            ]
-          }
-          """;
-
-  // The bad json string has no name, should return error
-  String badJsonString =
-      """
-          {
-            "constraints": [
-              {
-                "name": "OnKeyPressed",
-                "parameter": "SPACE"
-              }
-            ],
-            "actions": [
-              {
-                "name": "ChangeSceneAction",
-                "parameter": "Example Main Scene"
-              }
-            ]
-          }
-          """;
 
   @BeforeEach
   void setUp() {
@@ -67,6 +33,24 @@ class BehaviorParserTest {
 
   @Test
   void parse_validJson_success() throws JsonProcessingException, ParsingException {
+    String goodJsonString =
+        """
+            {
+              "Name": "SceneChanger",
+              "constraints": [
+                {
+                  "name": "KeyPressConstraint",
+                  "parameter": "SPACE"
+                }
+              ],
+              "actions": [
+                {
+                  "name": "ChangeSceneAction",
+                  "parameter": "Example Main Scene"
+                }
+              ]
+            }
+            """;
     JsonNode node = myMapper.readTree(goodJsonString);
     Behavior behavior = myBehaviorParser.parse(node);
     assertNotNull(behavior); // Once we have actual behavior classes this should work
@@ -76,12 +60,160 @@ class BehaviorParserTest {
   }
 
   @Test
-  void parse_invalidJson_failure() throws JsonProcessingException, ParsingException {
-    JsonNode node = myMapper.readTree(badJsonString);
-    assertThrows(ParsingException.class, () -> myBehaviorParser.parse(node)); // TODO: Super weird error
+  void parse_validJsonRepeatConstraints_success() throws JsonProcessingException, ParsingException {
+    String repeatConstraintsJsonString =
+        """
+            {
+              "Name": "SceneChanger",
+              "constraints": [
+                {
+                  "name": "KeyPressConstraint",
+                  "parameter": "SPACE"
+                },
+                {
+                  "name": "KeyPressConstraint",
+                  "parameter": "SPACE"
+                }
+              ],
+              "actions": [
+                {
+                  "name": "ChangeSceneAction",
+                  "parameter": "Example Main Scene"
+                }
+              ]
+            }
+            """;
+
+    JsonNode node = myMapper.readTree(repeatConstraintsJsonString);
+    Behavior behavior = myBehaviorParser.parse(node);
+
+    assertNotNull(behavior);
+
+    behavior
+        .getSerializedFields()
+        .forEach(Assertions::assertNotNull);
+
+    assertFalse(behavior
+        .getSerializedFields()
+        .get(CONSTRAINTS_INDEX).toString().contains(",")); // Checks that list only has one element
   }
 
-  // TODO: This test has similar structure to Component but we do not have concrete behaviors yet
+  @Test
+  void parse_validJsonNoConstraints_success() throws JsonProcessingException, ParsingException {
+    String noConstraintsJson =
+        """
+            {
+              "Name": "SceneChanger",
+              "constraints": [
+              ],
+              "actions": [
+                {
+                  "name": "ChangeSceneAction",
+                  "parameter": "Example Main Scene"
+                }
+              ]
+            }
+            """;
+    JsonNode node = myMapper.readTree(noConstraintsJson);
+    Behavior behavior = myBehaviorParser.parse(node);
+
+    assertNotNull(behavior);
+
+    behavior
+        .getSerializedFields()
+        .forEach(Assertions::assertNotNull);
+  }
+
+  @Test
+  void parse_validJsonRepeatActions_success() throws JsonProcessingException, ParsingException {
+    String repeatActionsJsonString =
+        """
+            {
+              "Name": "SceneChanger",
+              "constraints": [
+                {
+                  "name": "KeyPressConstraint",
+                  "parameter": "SPACE"
+                }
+              ],
+              "actions": [
+                {
+                  "name": "ChangeSceneAction",
+                  "parameter": "Example Main Scene"
+                },
+                {
+                  "name": "ChangeSceneAction",
+                  "parameter": "Example Main Scene"
+                }
+              ]
+            }
+            """;
+    JsonNode node = myMapper.readTree(repeatActionsJsonString);
+    Behavior behavior = myBehaviorParser.parse(node);
+
+    assertNotNull(behavior);
+
+    behavior
+        .getSerializedFields()
+        .forEach(Assertions::assertNotNull);
+
+    assertFalse(behavior
+        .getSerializedFields()
+        .get(ACTIONS_INDEX).toString().contains(",")); // Checks that list only has one element
+  }
+
+  @Test
+  void parse_validJsonNoActions_success() throws JsonProcessingException, ParsingException {
+    String noActionsJsonString =
+        """
+            {
+              "Name": "SceneChanger",
+              "constraints": [
+                {
+                  "name": "KeyPressConstraint",
+                  "parameter": "SPACE"
+                }
+              ],
+              "actions": [
+              ]
+            }
+            """;
+    JsonNode node = myMapper.readTree(noActionsJsonString);
+    Behavior behavior = myBehaviorParser.parse(node);
+
+    assertNotNull(behavior);
+
+    behavior
+        .getSerializedFields()
+        .forEach(Assertions::assertNotNull);
+  }
+
+  @Test
+  void parse_invalidJson_failure() throws JsonProcessingException, ParsingException {
+    // The bad json string has no name, should return error
+    String badJsonString =
+        """
+            {
+              "constraints": [
+                {
+                  "name": "OnKeyPressed",
+                  "parameter": "SPACE"
+                }
+              ],
+              "actions": [
+                {
+                  "name": "ChangeSceneAction",
+                  "parameter": "Example Main Scene"
+                }
+              ]
+            }
+            """;
+
+    JsonNode node = myMapper.readTree(badJsonString);
+    assertThrows(ParsingException.class, () -> myBehaviorParser.parse(node));
+  }
+
+  // TODO: Create write function
   @Test
   void write_validComponent_success() throws IOException {
 //    Transform transformComponent = new Transform();
