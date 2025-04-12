01ae5568857c2538d73fa421819597e6761af7f3
Christian Bepler <cgb45@duke.edu>
2025-04-12 16:55:02 -0400

    CLEAN-01: remove unused import

--- src/main/java/oogasalad/view/scene/MainMenuScene.java
+++ src/main/java/oogasalad/view/scene/MainMenuScene.java
@@ -6,7 +6,6 @@ import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
-import javafx.scene.text.Font;
 import oogasalad.model.config.GameConfig;
 
 /**
