bff77170e3dd4d7952a5b3e227ff9fbd8cb1a0ed
Reyan Shariff <rts34@duke.edu>
2025-04-13 15:28:56 -0400

    Refactor CLEAN-01: Reduce cyclomatic complexity in DeserializedFieldUIFactory

--- src/main/java/oogasalad/view/gui/deserializedFieldInput/DeserializedFieldUIFactory.java
+++ src/main/java/oogasalad/view/gui/deserializedFieldInput/DeserializedFieldUIFactory.java
@@ -10,6 +10,14 @@ public class DeserializedFieldUIFactory {
   public static HBox createDeserializedFieldUI(SerializedField<?> field) {
     String fieldType = field.getFieldType().getSimpleName();
 
+    if (fieldType.equals("List")) {
+      return createListInputBox(field);
+    }
+
+    return createSimpleInputBox(fieldType, field);
+  }
+
+  private static HBox createSimpleInputBox(String fieldType, SerializedField<?> field) {
     switch (fieldType) {
       case "Double", "double" -> {
         DoubleFieldInput box = new DoubleFieldInput();
@@ -21,44 +29,41 @@ public class DeserializedFieldUIFactory {
         box.initGUI(field);
         return box;
       }
-      case "List" -> {
-        Type genericType = field.getFieldGenericType();
-
-        if (genericType instanceof ParameterizedType pt) {
-          String simpleName = pt.getActualTypeArguments()[0].getTypeName().replaceAll(".*\\.", "");
-
-          switch (simpleName) {
-            case "String" -> {
-              StringListFieldInput box = new StringListFieldInput();
-              box.initGUI(field);
-              return box;
-            }
-            case "Behavior" -> {
-              BehaviorListFieldInput box = new BehaviorListFieldInput();
-              box.initGUI(field);
-              return box;
-            }
-            case "BehaviorConstraint<?>" -> {
-              ConstraintListFieldInput box = new ConstraintListFieldInput();
-              box.initGUI(field);
-              return box;
-            }
-            case "BehaviorAction<?>" -> {
-              ActionListFieldInput box = new ActionListFieldInput();
-              box.initGUI(field);
-              return box;
-            }
-            default -> throw new IllegalStateException("Unsupported List generic type: " + simpleName);
-          }
-        } else {
-          throw new IllegalStateException("Expected ParameterizedType for List");
-        }
-      }
-      default -> {
-        throw new IllegalStateException("Unexpected value: " + fieldType);
-      }
+      default -> throw new IllegalStateException("Unsupported field type: " + fieldType);
     }
   }
 
+  private static HBox createListInputBox(SerializedField<?> field) {
+    Type genericType = field.getFieldGenericType();
 
+    if (!(genericType instanceof ParameterizedType pt)) {
+      throw new IllegalStateException("Expected ParameterizedType for List");
+    }
+
+    String simpleName = pt.getActualTypeArguments()[0].getTypeName().replaceAll(".*\\.", "");
+
+    return switch (simpleName) {
+      case "String" -> {
+        StringListFieldInput box = new StringListFieldInput();
+        box.initGUI(field);
+        yield box;
+      }
+      case "Behavior" -> {
+        BehaviorListFieldInput box = new BehaviorListFieldInput();
+        box.initGUI(field);
+        yield box;
+      }
+      case "BehaviorConstraint<?>" -> {
+        ConstraintListFieldInput box = new ConstraintListFieldInput();
+        box.initGUI(field);
+        yield box;
+      }
+      case "BehaviorAction<?>" -> {
+        ActionListFieldInput box = new ActionListFieldInput();
+        box.initGUI(field);
+        yield box;
+      }
+      default -> throw new IllegalStateException("Unsupported List generic type: " + simpleName);
+    };
+  }
 }
