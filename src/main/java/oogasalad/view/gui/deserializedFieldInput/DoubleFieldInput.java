89fc314f63b085bccff5a58eb0f63e33c54971c4
Reyan Shariff <rts34@duke.edu>
2025-04-15 00:05:26 -0400

    Doc: Add Javadoc Comment to DoubleFieldInput

--- src/main/java/oogasalad/view/gui/deserializedFieldInput/DoubleFieldInput.java
+++ src/main/java/oogasalad/view/gui/deserializedFieldInput/DoubleFieldInput.java
@@ -8,6 +8,11 @@ import javafx.scene.layout.Priority;
 import oogasalad.model.engine.base.serialization.SerializedField;
 import oogasalad.view.gui.textField.DoubleTextField;
 
+/**
+ * A class that provides a JavaFX UI component for editing a double field in a serialized object.
+ * It extends the DeserializedFieldUI class and implements the showGUI method to create the UI.
+ * The UI consists of a label and a text field that only accepts double values.
+ */
 public class DoubleFieldInput extends DeserializedFieldUI<Double> {
 
   @Override
