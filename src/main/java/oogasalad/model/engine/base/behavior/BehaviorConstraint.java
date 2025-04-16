ac331e6b4a30fd2e4d56a09a20feba449dd42dc7
Hsuan-Kai Liao <hl475@duke.edu>
2025-04-15 22:01:42 -0400

    Refactor CLEAN-01: remove duplicated code inside BehaviorComponent

--- src/main/java/oogasalad/model/engine/base/behavior/BehaviorConstraint.java
+++ src/main/java/oogasalad/model/engine/base/behavior/BehaviorConstraint.java
@@ -1,8 +1,6 @@
 package oogasalad.model.engine.base.behavior;
 
-import oogasalad.model.engine.base.architecture.GameComponent;
 import oogasalad.model.engine.base.serialization.Serializable;
-import oogasalad.model.engine.base.serialization.SerializableField;
 
 /**
  * The BehaviorConstraint class is the base class for all behavior action constraints. Constraints are
@@ -11,46 +9,7 @@ import oogasalad.model.engine.base.serialization.SerializableField;
  * @param <T> the type of the parameter that the constraint will check against
  * @author Hsuan-Kai Liao 
  */
-public abstract class BehaviorConstraint<T> implements Serializable, BehaviorComponent {
-  @SerializableField
-  private T parameter;
-
-  private Behavior behavior;
-
-  /**
-   * Set the behavior of the constraint. This is used to set the behavior that the constraint belongs to
-   * @param behavior the behavior to set
-   */
-  public final void setBehavior(Behavior behavior) {
-    this.behavior = behavior;
-  }
-
-  /**
-   * Get the parameter of the constraint. This is used to get the parameter that the constraint will
-   */
-  public final T getParameter() {
-    return parameter;
-  }
-
-  /**
-   * Set the parameter of the constraint. This is used to set the parameter that the constraint will
-   * check against.
-   *
-   * @param parameter the parameter to set
-   */
-  public final void setParameter(T parameter) {
-    this.parameter = parameter;
-  }
-
-  /**
-   * Get the component based on
-   *
-   * @param componentClass the component class specified
-   * @return the component instance
-   */
-  public final <U extends GameComponent> U getComponent(Class<U> componentClass) {
-    return behavior.getController().getComponent(componentClass);
-  }
+public abstract class BehaviorConstraint<T> extends BehaviorComponent<T> implements Serializable {
 
   /**
    * Check if the constraint is met.
@@ -67,34 +26,9 @@ public abstract class BehaviorConstraint<T> implements Serializable, BehaviorCom
   }
 
   /**
-   * This method is called to get the default parameter.
-   * @return the default parameter
-   */
-  protected abstract T defaultParameter();
-
-  /**
    * Check if the constraint is met.
    * @param parameter the parameter to check against
    * @return true if the constraint is met, false otherwise
    */
   protected abstract boolean check(T parameter);
-
-  /**
-   * This method is called to get the component references.
-   */
-  protected void awake() {
-    // NOTE: This method should be override if needed
-  }
-
-  /**
-   * Check if two BehaviorConstraint objects are equal
-   * 
-   * @param other the other BehaviorConstraint object to compare against
-   * 
-   * @return true if the two BehaviorConstraint objects are equal, false otherwise
-   */
-  public boolean equals(BehaviorConstraint<?> other) {
-    return this.getClass().equals(other.getClass());
-  }
-
 }
